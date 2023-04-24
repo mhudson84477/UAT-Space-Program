4930bd78335424013993c5c3fad2b3b28ceb3fa9
mhudson84477 <129351293+mhudson84477@users.noreply.github.com>
2023-04-23 22:46:42 -0700

    Add files via upload

---
+++ countdown loop.js
@@ -0,0 +1,73 @@
+
+function disablelaunch()
+{
+    document.getElementById("Launch").disabled = true;//turns off launch button
+    document.getElementById("Cancel").disabled = false;//turns on cancel button
+}
+
+function disablecancel()
+{
+    document.getElementById("Launch").disabled = false;//turns on launch button
+    document.getElementById("Cancel").disabled = true;//turns off cancel button
+}
+
+
+var countingDown = false;
+async function countDown() 
+//i added the alert for thr "Countdown initiated" here
+{alert("WARNING: COUNTDOWN INITIATED");
+    /*  for (initial condition, ending condition - stop when not true, what to after each loop) i++ same as  i = i+1*/
+    countingDown = true;
+    for (var currTime = 10; countingDown && currTime >= 0; currTime--) {
+        document.getElementById("countdownTimer").innerHTML = "TIME REMAINING: " + currTime;
+        await new Promise(timer => setTimeout(timer, 1000));
+    }
+    if (countingDown) {
+        document.getElementById("countdownTimer").innerHTML = "Blastoff!";
+    } else {
+        document.getElementById("countdownTimer").innerHTML = "Count Down Cancelled!";
+    }
+}
+function cancelCountDown () {
+    countingDown = false;
+}
+function checkCreds()
+{
+ alert("VERIFYING LOGIN INFORMATION, PLEASE STAND BY")
+
+var firstname = document.getElementById("firstname").value;
+var lastname = document.getElementById("lastname").value;
+var CIN = document.getElementById("idnum").value;
+var fullname = firstname + " " + lastname;
+
+//this is where I added the rules for the log-in page
+if(fullname.length > 20 || fullname.length <4)
+{
+    document.getElementById("loginstatus").innerHTML = "Invalid Name";
+}
+if(CIN >10 || CIN <2)
+    {
+        document.getElementById("loginstatus").innerHTML = "Invalid Identification Number";
+    }
+
+else if(fullname.length <= 20 || fullname.length <= 4)
+    {
+    document.getElementById("loginstatus").innerHTML = "PROCESSING" + fullname + " " + CIN;
+    alert("ACCESS GRANTED");
+    location.replace("Page 2.html")
+    }
+}
+
+//this is where I added my countdown sound
+function begincountdown()
+{
+mysound = new sound("countdown.mp3")
+mysound.play();
+}
+function sound(src)
+{
+    this.sound = document.createElement("audio")
+    this.sound.src = src
+    this.play = function(){
+        this.sound.play();}
+}
